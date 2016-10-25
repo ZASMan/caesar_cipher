09f40727a43e283df08eac8aff80f1b6ffced19f
Zane <zane.sandin@luav.com>
2016-10-25 12:48:04 -0400

    first commit

---
+++ caesar_cypher.rb
@@ -0,0 +1,15 @@
+=begin
+In cryptography, a Caesar cipher, also known as Caesar's cipher, the shift cipher, Caesar's code or Caesar shift, is one of the simplest and most widely known encryption techniques. It is a type of substitution cipher in which each letter in the plaintext is replaced by a letter some fixed number of positions down the alphabet. For example, with a left shift of 3, D would be replaced by A, E would become B, and so on. The method is named after Julius Caesar, who used it in his private correspondence.
+
+caesar_cypher("What a string!", 5)
+"Bmfy f xywnsl!"
+=end
+
+def caesar_cypher(string, n)
+	plain = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+	#Break String into an array of letters
+	string.split("")
+	#Move the index n amount of places to the left
+
+
+end
