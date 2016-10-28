ff3ffc7d680dcd3e30af94a3949bcc38dfe6fa9e
Zane <zane.sandin@luav.com>
2016-10-28 15:21:32 -0400

    caesar cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -22,15 +22,36 @@ def caesar_cipher(str, n)
 	str_letters.each do |letter|
 		#Check if alphabet array has letter from string
 		if alphabet.include?(letter)
+			#Finds where the index of the letter is in the alphabet, then add n to get the sum of the letter_index.
+			#So if letter is "p", it would be at the 15 index in the alphabet. If n = 5,
+			#5 added it would be 20. So letter_index = 20, which would put the letter "u" into the cipher
 			letter_index = alphabet.find_index(letter) + n
-			letter_index = letter_index - alphabet.length if letter_index >= alphabet.length
+			puts "letter index is less than " + alphabet.length.to_s + ", so letter_index is " + letter_index.to_s
+			puts "The corresponding letter for this is " + alphabet[letter_index].to_s
+			if letter_index >= alphabet.length
+				#If the letter index is greater than or equal to 26, change the
+				#letter_index to a letter within the alphabet. So, if letter is "p" and n is equal to 25
+				#the letter_index will be equal to 40 (while the alphabet array length is 26)
+				#40 - 25 will put the letter index to 15. The 15 index of the alphabet will be "o", 
+				#which will be inserted into the cipher
+				letter_index = letter_index - alphabet.length
+				puts "letter index is greater than " + alphabet.length.to_s + ", so it is " + letter_index.to_s
+				puts "The corresponding letter is " + alphabet[letter_index].to_s
+			end
+			#The resulting letter is added to the blank cipher string
 			cipher += alphabet[letter_index]
 		end
 	end
-	puts cipher
+	cipher
+	puts "The final encrypted cipher is " + cipher
+	puts "_____________________________________________________________________________"
+
 end
 
+puts "The result for the string 'pizza' with a shift factor of 25 is"
+caesar_cipher("pizza", 25)
+puts "The result for the string 'pizza' with a shift factor of 26 is"
+caesar_cipher("pizza", 26)
 puts "The result for the string 'pizza' with a shift factor of 5 is"
 caesar_cipher("pizza", 5)
-puts "The result for the string 'America' with a shift factor of 10 is"
-caesar_cipher("America", 10)
+
